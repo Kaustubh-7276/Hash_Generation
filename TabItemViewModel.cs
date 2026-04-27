bb1b567685f6406ad2af178cc5e76897d77b215a
itskaustubhtodmal <kaustubhtodmal@gmail.com>
2026-04-27 15:02:24 +0530

    Create TabItemViewModel.cs

---
+++ TabItemViewModel.cs
@@ -0,0 +1,8 @@
+namespace HashGenerator.VIewModel
+{
+    public class TabItemViewModel : ViewModelBase
+    {
+        public string Title { get; set; } = "New Tab";
+        public string Content { get; set; } = "Offline Content Here";
+    }
+}
