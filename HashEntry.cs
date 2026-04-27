cd64509163f3df61ef0e243ebbac1d98ee1ff3a0
itskaustubhtodmal <kaustubhtodmal@gmail.com>
2026-04-27 14:58:53 +0530

    Create HashEntry.cs

---
+++ HashEntry.cs
@@ -0,0 +1,8 @@
+namespace HashGenerator.VIewModel
+{
+    public class HashEntry
+    {
+        public string RawText { get; set; } = string.Empty;
+        public string HashResult { get; set; } = string.Empty;
+    }
+}
