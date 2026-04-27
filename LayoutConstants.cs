89f5e3fc96d5edad2bec64fa38f4338c3b2f9480
itskaustubhtodmal <kaustubhtodmal@gmail.com>
2026-04-27 14:52:01 +0530

    Create LayoutConstants.cs

---
+++ LayoutConstants.cs
@@ -0,0 +1,11 @@
+namespace HashGenerator.Services
+{
+    public static class LayoutConstants
+    {
+        public const double PageWidth = 793;
+        public const double PageHeight = 1122;
+
+        public const double Margin = 40;
+        public const int RowsPerPage = 12; // adjust for content
+    }
+}
